d1c3059ae55bd3f613daacb2070af6af5b75ea51
meghanasarikonda <meghana94987@gmail.com>
2018-11-12 00:21:04 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import nav from "./nav";
-import { top, bottom } from "./footer";
+import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 
 const button = makeButton('Yay a button!!!!');
+button.style = makeColorStyle("red");
 document.body.appendChild(button);
+document.body.appendChild(footer);
